c769dd720516c50eacc9aa68ac87fd33782ad92e
Spiros Dimopulos <spiros@wantedpixel.com>
2019-07-26 12:09:11 +0200

    Create test.js

---
+++ test.js
@@ -0,0 +1,340 @@
+(function(undefined) {
+  runReactApplication({
+  path: "/product-comparison/die-besten-loremipsum-2019-im-vergleich",
+  meta: {
+    pageTitle: "lorem ipsum",
+    description: "lorem ipsum",
+    canonical: "https://"
+  },
+  aboveTable: {
+    headlineH1: "lorem ipsum",
+    textDesktop: "<p>lorem ipsum.</p>",
+    headlineH2: "lorem ipsum:"
+  },
+  products: {
+    ids: ["6181640", "6519074", "6067569", "6555445", "5738844", "6555440", "5587365"],
+    images: ["https://cdn.idealo.com/folder/Product/5587/3/5587365/s1_produktbild_gross/ilife-v5s-pro.jpg",
+      "https://cdn.dsf.com/folder/Product/6555/4/6555445/s1_produktbild_gross/roborock-s6-staubsauger-roboter.jpg",
+      "https://cdn.sdf.com/folder/Product/5738/8/5738844/s1_produktbild_gross/dyson-v8-absolute-227296-01.jpg",
+      "https://cdn.sdf.com/folder/Product/6555/4/6555440/s1_produktbild_gross/roborock-s6-staubsauger-roboter-weiss.jpg",
+      "https://cdn.sfd.com/folder/Product/6181/6/6181640/s1_produktbild_gross/gorenje-rk-4172-an.jpg",
+      "https://cdn.sdf.com/folder/Product/6067/5/6067569/s1_produktbild_gross/dyson-cyclone-v10-absolute.jpg",
+      "https://cdn.sdf.com/folder/Product/6519/0/6519074/s1_produktbild_gross/dyson-cyclone-v11-absolute.jpg"]
+  },
+  hightlightedOffers: {
+    bestPriceIndex: 1,
+    bestRatioIndex: 4
+  },
+  comparisonTableRows: {
+    mainInfoRows: [
+      {
+        rowTitle: {rowTitleType: "simpleText", rowTitleContent: "lorem ipsum"},
+        cells: [
+          {
+            cellType: "list",
+            cellContent: "lorem ipsum"
+          },
+          {
+            cellType: "list",
+            cellContent: "lorem ipsum"
+          },
+          {
+            cellType: "list",
+            cellContent: "lorem ipsum"
+          },
+          {
+            cellType: "list",
+            cellContent: "lorem ipsum"
+          },
+          {
+            cellType: "list",
+            cellContent: "lorem ipsum"
+          },
+          {
+            cellType: "list",
+            cellContent: "lorem ipsum"
+          },
+          {
+            cellType: "list",
+            cellContent: "lorem ipsum"
+          }
+        ]
+      },
+      {
+        rowTitle: {rowTitleType: "simple text", rowTitleContent: "lorem ipsum"},
+        cells: [
+          {
+            cellType: "two lines with tooltip",
+            cellContent: ["lorem ipsum", "lorem ipsum"],
+            cellTooltipContent: ["lorem ipsum dolor sit amet"]
+          },
+          {
+            cellType: "simple text",
+            cellContent: "lorem ipsum"
+          },
+          {
+            cellType: "simple text",
+            cellContent: "mit Beutel"
+          },
+          {
+            cellType: "two lines with tooltip",
+            cellContent: ["ohne Beutel", "Zyklon Technologie"],
+            cellTooltipContent: ["lorem ipsum dolor sit amet"]
+          },
+          {
+            cellType: "simple text",
+            cellContent: "mit Beutel"
+          },
+          {
+            cellType: "simple text",
+            cellContent: "mit Beutel"
+          },
+          {
+            cellType: "two lines with tooltip",
+            cellContent: ["ohne Beutel", "Zyklon Technologie"],
+            cellTooltipContent: ["lorem ipsum dolor sit amet"]
+          }
+        ]
+      },
+      {
+        rowTitle: {rowTitleType: "simple text", rowTitleContent: "Vor- und Nachteile"},
+        cells: [
+          {
+            cellType: "pros and cons list",
+            cellContent: {
+              pros: "vielseitig einsetzbar dank großem Lieferumfang||besonders wartungsarmer Filter||besonders wartungsarmer Filter||der Staubsauger richtet sich von selbst wieder auf, wenn er umgekippt ist",
+              cons: "relativ hohe Anschaffungskosten"
+            },
+          },
+          {
+            cellType: "pros and cons list",
+            cellContent: {
+              pros: "vielseitig einsetzbar dank großem Lieferumfang||besonders wartungsarmer Filter||besonders wartungsarmer Filter||der Staubsauger richtet sich von selbst wieder auf, wenn er umgekippt ist",
+              cons: "relativ hohe Anschaffungskosten"
+            },
+          },
+          {
+            cellType: "pros and cons list",
+            cellContent: {
+              pros: "langes Kabel||Leuchtanzeige bei vollem Behälter",
+              cons: "relativ hohe Anschaffungskosten"
+            },
+          },
+          {
+            cellType: "pros and cons list",
+            cellContent: {
+              pros: "vielseitig einsetzbar dank großem Lieferumfang||besonders wartungsarmer Filter||besonders wartungsarmer Filter||der Staubsauger richtet sich von selbst wieder auf, wenn er umgekippt ist",
+              cons: "Staubsauger benötigt relativ viel Abstellfläche"
+            },
+          },
+          {
+            cellType: "pros and cons list",
+            cellContent: {
+              pros: "vielseitig einsetzbar dank großem Lieferumfang||besonders wartungsarmer Filter||besonders wartungsarmer Filter||der Staubsauger richtet sich von selbst wieder auf, wenn er umgekippt ist",
+              cons: "relativ hohe Anschaffungskosten"
+            },
+          },
+          {
+            cellType: "pros and cons list",
+            cellContent: {
+              pros: "vielseitig einsetzbar dank großem Lieferumfang||besonders wartungsarmer Filter||besonders wartungsarmer Filter||der Staubsauger richtet sich von selbst wieder auf, wenn er umgekippt ist",
+              cons: "relativ hohe Anschaffungskosten"
+            },
+          },
+          {
+            cellType: "pros and cons list",
+            cellContent: {
+              pros: "vielseitig einsetzbar dank großem Lieferumfang||besonders wartungsarmer Filter||besonders wartungsarmer Filter||der Staubsauger richtet sich von selbst wieder auf, wenn er umgekippt ist",
+              cons: "relativ hohe Anschaffungskosten"
+            },
+          }
+        ]
+      }
+    ],
+    rowGroups: [
+      {
+        groupTitle: "Effizienz und Leistung",
+        rows: [
+          {
+            rowTitle: {rowTitleType: "with tooltip", rowTitleContent: "max. Leistung", rowTooltipContent: "lorem ipsum dolor sit amet"},
+            cells: [
+              {
+                cellType: "two lines with plus signs",
+                cellContent: "700 Watt",
+                plusSignsCount: "2"
+              },
+              {
+                cellType: "two lines with plus signs",
+                cellContent: "800 Watt",
+                plusSignsCount: "3"
+              },
+              {
+                cellType: "two lines with plus signs",
+                cellContent: "850 Watt",
+                plusSignsCount: "3"
+              },
+              {
+                cellType: "two lines with plus signs",
+                cellContent: "700 Watt",
+                plusSignsCount: "2"
+              },
+              {
+                cellType: "two lines with plus signs",
+                cellContent: "600 Watt",
+                plusSignsCount: "1"
+              },
+              {
+                cellType: "two lines with plus signs",
+                cellContent: "1000 Watt",
+                plusSignsCount: "4"
+              },
+              {
+                cellType: "two lines with plus signs",
+                cellContent: "700 Watt",
+                plusSignsCount: "2"
+              }
+            ]
+          },
+          {
+            rowTitle: {rowTitleType: "simple text", rowTitleContent: "Geräuschentwick-lung in Dezibel (dB)"},
+            cells: [
+              {
+                cellType: "simple text",
+                cellContent: "80 db"
+              },
+              {
+                cellType: "simple text",
+                cellContent: "76 db"
+              },
+              {
+                cellType: "simple text",
+                cellContent: "60 db"
+              },
+              {
+                cellType: "simple text",
+                cellContent: "65 db"
+              },
+              {
+                cellType: "simple text",
+                cellContent: "80 db"
+              },
+              {
+                cellType: "simple text",
+                cellContent: "80 db"
+              },
+              {
+                cellType: "simple text",
+                cellContent: "60 db"
+              }
+            ]
+          },
+          {
+            rowTitle: {rowTitleType: "simple text", rowTitleContent: "Geräuschpegel"},
+            cells: [
+              {
+                cellType: "simple text",
+                cellContent: "laut"
+              },
+              {
+                cellType: "simple text",
+                cellContent: "eher leise"
+              },
+              {
+                cellType: "simple text",
+                cellContent: "leise"
+              },
+              {
+                cellType: "simple text",
+                cellContent: "besonders leise"
+              },
+              {
+                cellType: "simple text",
+                cellContent: "laut"
+              },
+              {
+                cellType: "simple text",
+                cellContent: "eher leise"
+              },
+              {
+                cellType: "simple text",
+                cellContent: "laut"
+              }
+            ]
+          }
+        ]
+      },
+      {
+        groupTitle: "Technische Daten",
+        rows: [
+          {
+            rowTitle: {rowTitleType: "simple text", rowTitleContent: "Behältervolumen"},
+            cells: [
+              {
+                cellType: "simple text",
+                cellContent: "0,8 Liter"
+              },
+              {
+                cellType: "simple text",
+                cellContent: "2,8 Liter"
+              },
+              {
+                cellType: "simple text",
+                cellContent: "4,8 Liter"
+              },
+              {
+                cellType: "simple text",
+                cellContent: "2,8 Liter"
+              },
+              {
+                cellType: "simple text",
+                cellContent: "1,2 Liter"
+              },
+              {
+                cellType: "simple text",
+                cellContent: "5,4 Liter"
+              },
+              {
+                cellType: "simple text",
+                cellContent: "6,9 Liter"
+              }
+            ]
+          },
+          {
+            rowTitle: {rowTitleType: "with tooltip", rowTitleContent: "HEPA-Filter", rowTooltipContent: "lorem ipsum dolor"},
+            cells: [
+              {
+                cellType: "tick",
+                cellContent: "true"
+              },
+              {
+                cellType: "tick",
+                cellContent: "false"
+              },
+              {
+                cellType: "tick",
+                cellContent: "true"
+              },
+              {
+                cellType: "tick",
+                cellContent: "true"
+              },
+              {
+                cellType: "tick",
+                cellContent: "false"
+              },
+              {
+                cellType: "tick",
+                cellContent: "true"
+              },
+              {
+                cellType: "tick",
+                cellContent: "true"
+              }
+            ]
+          }]
+      }
+    ]
+  }
+});
+
+})();
